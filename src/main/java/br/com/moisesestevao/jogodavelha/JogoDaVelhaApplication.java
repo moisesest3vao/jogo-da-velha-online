6fbdef6cf6cb98ee0054f07fcc012d66567c1558
moises <moisesestevao2004@hotmail.com>
2022-09-23 20:15:45 -0300

    initial commit

---
+++ src/main/java/br/com/moisesestevao/jogodavelha/JogoDaVelhaApplication.java
@@ -0,0 +1,13 @@
+package br.com.moisesestevao.jogodavelha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogoDaVelhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogoDaVelhaApplication.class, args);
+	}
+
+}
